f8adc18c80be9e58ae82959d0142e7c363c5f74b
Jérémy Marabel <jeremy.marabel@gmail.com>
2025-02-20 15:08:45 +0100

    feat(card-game): add options to select hiraganas (#4)
    * Added hiragana selection
    
    Added hiragana selection in option
    Added missing headers and back buttons
    
    * chore: Revamp the codebase (#29)
    
    * chore: split files and rearrange codebase
    
    * chore: css files were inverted
    
    ---------
    
    Co-authored-by: Nathan Pichon <nathan.pichon@strapi.io>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import GameModeSelector from "./GameModeSelector";
-import HiraganaCard from "./HiraganaCard";
-import Game from "./Game";
+import GameModeSelector from "./components/game/GameModeSelector";
+import GameOptions from "./components/game-options/GameOptions";
+import Game from "./components/game/Game";
+import { useEffect } from "react";
+import { HiraganaChar, hiraganaData } from "./services/hiragana-characters";
+
+import "./assets/shared.css";
 
 export default function App() {
+  // Initialize localStorage with base hiragana if not exists
+  useEffect(() => {
+    if (!localStorage.getItem("selectedHiragana")) {
+      const baseCharacters = hiraganaData.hiragana.map(
+        (c: HiraganaChar) => c.character,
+      );
+      localStorage.setItem("selectedHiragana", JSON.stringify(baseCharacters));
+    }
+  }, []);
+
   return (
     <Router>
       <Routes>
@@ -11,6 +25,7 @@ export default function App() {
         <Route path="/hiragana-quiz" element={<Game mode="hiragana" />} />
         <Route path="/reverse-mode" element={<Game mode="romaji" />} />
         <Route path="/mixed-quiz" element={<Game mode="mixed" />} />
+        <Route path="/options" element={<GameOptions />} />
       </Routes>
     </Router>
   );
